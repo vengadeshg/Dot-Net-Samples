d3314651cc80616b780af278af5da78491478fb4
CVHS2 <venkatesh2411@gmail.com>
2014-10-30 17:20:33 +0530

    rwegfr
    greg

---
+++ src/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace src
+{
+   public class Class1
+    {
+       public void met()
+       {
+           Console.WriteLine("Venkatesh");
+           Console.ReadLine();
+       }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.met();
+        }
+    }
+}
